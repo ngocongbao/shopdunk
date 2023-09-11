de7c923d30b7a04df4e7c99c991e148eca2a79d7
DESKTOP-UEFNE3E\PC <bao.ngo@idsolutions.com.vn>
2023-09-12 00:44:46 +0700

    update 11/09

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import banner from "../../assets/Banneriphone.jpg";
 export default function Home() {
-  const [Image, setImage] = useState(false);  
+  const [image, setImage] = useState(false);  
   useEffect(() => {
     const img = setTimeout(() => {
       setImage(true);
@@ -10,7 +10,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {Image && (
+      {image && (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black/[.5]">
           <div className="relative h-4/5">
             <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center absolute top-2 right-2" onClick={(e)=>setImage(false)}>
--- src/pages/iPad/index.jsx
+++ src/pages/iPad/index.jsx
@@ -1,8 +1,30 @@
-import React from "react";
-export default function IPad() {
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import banner1 from "../../assets/bannerhome1.png"
+import banner2 from "../../assets/bannerhome2.jpg"
+import banner3 from "../../assets/bannerhome3.jpg"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination} from 'swiper/modules';
+export default function Ipad() {
   return (
-    <div>
-      <h1>iPAD</h1>
-    </div>
-  );
+    <Swiper
+    modules={[Navigation, Pagination]}
+    spaceBetween={0}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    loop={true}
+    autoplay={{
+      delay: 1500,
+      disableOnInteraction: false,
+    }}
+    >
+       <SwiperSlide><img src={banner1} alt=''/></SwiperSlide>
+       <SwiperSlide><img src={banner2} alt=''/></SwiperSlide>
+       <SwiperSlide><img src={banner3} alt=''/></SwiperSlide>
+    </Swiper>
+  )
 }
